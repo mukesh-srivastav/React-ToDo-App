15edebbd054d97d51005d0b3a92642554bc0a89d
Mukesh Srivastava <mukeshsrivastav691@gmail.com>
2019-01-10 15:29:49 +0530

    Import export, default import export files/functions from user defined files

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,63 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__utils_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__person_js__ = __webpack_require__(2);
+
+
 
 console.log('App.js is running!');
+
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["c" /* square */](3));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["a" /* add */](3,4));
+console.log(__WEBPACK_IMPORTED_MODULE_0__utils_js__["b" /* default */](4,3)); // with default naming is not necessary to be same. 
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["c" /* isAdult */](19));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["a" /* canDrink */](23));
+console.log(__WEBPACK_IMPORTED_MODULE_1__person_js__["b" /* default */](65));
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return square; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return add; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return subtract; });
+console.log('utils.js is running');
+
+const square = (x) => x*x;
+const add = (a, b) => a + b;
+const subtract = (a,b) => a-b;
+
+
+
+// export const square = (x) => x*x;
+// export const add = (a, b) => a + b;
+// const subtract = (a,b) => a-b;
+
+// export default subtract;
+// export default (a,b) => a-b;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return isAdult; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return canDrink; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return isSenior; });
+const isAdult = (age) => age > 18;
+const canDrink = (age) => age > 21;
+
+const isSenior = (age) => age > 60;
+
+
+
+
 
 /***/ })
 /******/ ]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+import subtraction, { square, add } from './utils.js'
+import isSeniorCitizen, { isAdult, canDrink} from './person.js'
+
 console.log('App.js is running!');
+
+console.log(square(3));
+console.log(add(3,4));
+console.log(subtraction(4,3)); // with default naming is not necessary to be same. 
+console.log(isAdult(19));
+console.log(canDrink(23));
+console.log(isSeniorCitizen(65));
